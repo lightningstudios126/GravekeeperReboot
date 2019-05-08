405cb7ea7ed106c917a90778950e6b54ce73ec49
Fungeey <aaron@abcrlee.com>
2019-05-08 14:20:31 -0400

    Added Player and Tag enum

---
+++ GravekeeperReboot/Source/Entities/Player.cs
@@ -0,0 +1,22 @@
+﻿using Components;
+using Microsoft.Xna.Framework.Graphics;
+using Nez;
+using Nez.Sprites;
+
+namespace Entities {
+    public class Player{
+        public Entity PlayerEntity { get;  private set; }
+
+        private Player() {}
+
+        public static Player CreatePlayer (Scene scene) {
+            Player player = new Player();
+            player.PlayerEntity = scene.createEntity("Player");
+            player.PlayerEntity.setTag((int)Tags.Player);
+            player.PlayerEntity.addComponent(new Sprite(scene.content.Load<Texture2D>(Content.Sprites.player)));
+            player.PlayerEntity.addComponent(new MoveComponent());
+
+            return player;
+        }
+    }
+}
