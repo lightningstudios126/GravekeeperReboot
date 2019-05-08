09aceef5a343f6938f3c971d5127549799a16d3a
Fungeey <aaron@abcrlee.com>
2019-05-08 14:23:21 -0400

    Implemented input - command system for user input

---
+++ GravekeeperReboot/Source/Components/MoveComponent.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Xna.Framework;
+using Nez;
+
+namespace Components {
+    public class MoveComponent : Component{
+        public Vector2? targetPosition;
+    }
+}
---
+++ GravekeeperReboot/Source/Input/Command.cs
@@ -0,0 +1,24 @@
+﻿using Nez;
+using Components;
+using Microsoft.Xna.Framework;
+
+namespace Input {
+    public class Command {
+        public Command() { }
+        public virtual void Execute() { }
+    }
+
+    public class MoveCommand : Command{
+        private Entity target;
+        private Vector2 position;
+       
+        public MoveCommand(Entity target, Vector2 position) {
+            this.target = target;
+            this.position = position;
+        }
+
+        public override void Execute() {
+            target.getComponent<MoveComponent>().targetPosition = position;
+        }
+    }
+}
