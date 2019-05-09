7c64877f03ee8744909ef6a1ebc648e15295b575
Fungeey <aaron@abcrlee.com>
2019-05-09 15:00:39 -0400

    Added wasd buttons to Input System

--- GravekeeperReboot/Source/Systems/InputSystem.cs
+++ GravekeeperReboot/Source/Systems/InputSystem.cs
@@ -8,19 +8,23 @@ using System;
 namespace GravekeeperReboot.Source.Systems {
     public class InputSystem : ProcessingSystem {
 		private CommandSystem commandSystem;
-		private Action AButton, BButton, CButton;
+		private Action WButton, AButton, SButton, DButton, CButton;
 
 		public InputSystem(Scene scene) : base() {
 			commandSystem = scene.getEntityProcessor<CommandSystem>();
-			AButton = () => commandSystem.AddCommand(new MoveCommand(scene.findEntitiesWithTag((int)Tags.Player)[0], new Vector2(10, 0)));
-			BButton = () => commandSystem.AddCommand(new MoveCommand(scene.findEntitiesWithTag((int)Tags.Player)[0], new Vector2(-10, 0)));
+			WButton = () => commandSystem.AddCommand(new MoveCommand(scene.findEntitiesWithTag((int)Tags.Player)[0], new Vector2(0, -10)));
+			AButton = () => commandSystem.AddCommand(new MoveCommand(scene.findEntitiesWithTag((int)Tags.Player)[0], new Vector2(-10, 0)));
+			SButton = () => commandSystem.AddCommand(new MoveCommand(scene.findEntitiesWithTag((int)Tags.Player)[0], new Vector2(0, 10)));
+			DButton = () => commandSystem.AddCommand(new MoveCommand(scene.findEntitiesWithTag((int)Tags.Player)[0], new Vector2(10, 0)));
 			CButton = () => commandSystem.AddCommand(new UndoCommand());
 		}
 
 		public override void process() {
-			if (Nez.Input.isKeyPressed(Keys.A)) AButton();
-			if (Nez.Input.isKeyPressed(Keys.B)) BButton();
-			if (Nez.Input.isKeyPressed(Keys.C)) CButton();
+			if (Input.isKeyPressed(Keys.W)) WButton();
+			if (Input.isKeyPressed(Keys.A)) AButton();
+			if (Input.isKeyPressed(Keys.S)) SButton();
+			if (Input.isKeyPressed(Keys.D)) DButton();
+			if (Input.isKeyPressed(Keys.C)) CButton();
 		}
 
 	}
