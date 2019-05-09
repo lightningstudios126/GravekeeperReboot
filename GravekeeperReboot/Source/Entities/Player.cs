576ae89f1d17ec0f0177f8cb7088fc76ae10aacf
Fungeey <aaron@abcrlee.com>
2019-05-09 10:34:04 -0400

    cleaned up player class again

--- GravekeeperReboot/Source/Entities/Player.cs
+++ GravekeeperReboot/Source/Entities/Player.cs
@@ -8,15 +8,15 @@ namespace Entities {
         public Entity PlayerEntity { get;  private set; }
 
         private Player(Entity playerEntity) {
-			this.PlayerEntity = playerEntity;
+			this.PlayerEntity = playerEntity;	
 		}
 
         public static Player CreatePlayer (Scene scene) {
 			Entity player;
             player = scene.createEntity("Player");
             player.setTag((int)Tags.Player);
-            player.addComponent(new Sprite(scene.content.Load<Texture2D>(Content.Sprites.Tiles.player)));
-            player.addComponent(new MoveComponent());
+			player.addComponent(new Sprite(scene.content.Load<Texture2D>(Content.Sprites.Tiles.player)))
+				.addComponent(new MoveComponent());
 
             return new Player(player);
         }
