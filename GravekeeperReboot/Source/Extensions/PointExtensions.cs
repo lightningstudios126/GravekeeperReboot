daff6a5286fcc63e29e3e7515cf747c955f304e9
Fungeey <aaron@abcrlee.com>
2019-05-13 19:54:15 -0400

    Added an extension class for Point

---
+++ GravekeeperReboot/Source/Extensions/PointExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace GravekeeperReboot.Source.Extensions {
+	static class PointExtensions {
+		public static Point scl(this Point point, int scalar) {
+			return new Point(point.X * scalar, point.Y * scalar);
+		}
+	}
+}
