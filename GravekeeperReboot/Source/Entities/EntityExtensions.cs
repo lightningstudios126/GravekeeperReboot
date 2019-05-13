677d77a7f423ab60b29fd0befed25db5428d5067
Fungeey <aaron@abcrlee.com>
2019-05-13 14:50:44 -0400

    Added HasComponent Extension Method

---
+++ GravekeeperReboot/Source/Entities/EntityExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Nez;
+
+namespace GravekeeperReboot.Source.Entities {
+	public static class EntityExtensions {
+		public static bool HasComponent<T>(this Entity entity) where T: Component {
+			return entity.getComponent<T>() != null;
+		}
+	}
+}
