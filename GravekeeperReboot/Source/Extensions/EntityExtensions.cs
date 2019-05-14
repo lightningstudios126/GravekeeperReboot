bf21922843de78e8e3a0e08257baf37d34a7b5d0
Fungeey <aaron@abcrlee.com>
2019-05-14 08:24:37 -0400

    Moved EntityExtensions class to Extensions folder

--- GravekeeperReboot/Source/Extensions/EntityExtensions.cs
+++ GravekeeperReboot/Source/Extensions/EntityExtensions.cs
@@ -1,6 +1,6 @@
 ﻿using Nez;
 
-namespace GravekeeperReboot.Source.Entities {
+namespace GravekeeperReboot.Source.Extensions {
 	public static class EntityExtensions {
 		public static bool HasComponent<T>(this Entity entity) where T: Component {
 			return entity.getComponent<T>() != null;
