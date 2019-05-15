d389b62e5a89d1528a96a7112c833d019c2e7478
Fungeey <aaron@abcrlee.com>
2019-05-14 20:57:01 -0400

    Refactored move and rotate system a bit

--- GravekeeperReboot/Source/Systems/MoveSystem.cs
+++ GravekeeperReboot/Source/Systems/MoveSystem.cs
@@ -18,25 +18,32 @@ namespace GravekeeperReboot.Source.Systems {
 
             foreach(Entity entity in entities) {
                 MoveComponent moveComponent = entity.getComponent<MoveComponent>();
-				if (entity.position != gameBoard.TileToWorldPosition(moveComponent.position)) {
-					entity.position = gameBoard.TileToWorldPosition(moveComponent.position);
+				Vector2 targetPosition = gameBoard.TileToWorldPosition(moveComponent.position);
+				if (entity.position != targetPosition) {
+					entity.position = targetPosition;
 
-					if (entity.HasComponent<GrabComponent>() && entity.getComponent<GrabComponent>().isGrabbing) {
-						GrabComponent grabComponent = entity.getComponent<GrabComponent>();
-						MoveComponent grabbedMoveComponent = grabComponent.target.getComponent<MoveComponent>();
-					
-						Point offset = moveComponent.position - grabbedMoveComponent.position;
-						if (offset == Point.Zero) // Player overlaps with target when pushing forward
-							offset = Utilities.Direction.DirectionPointOffset(entity.getComponent<RotateComponent>().direction);
-						else
-							offset = offset.Normalize();
-						
-						CommandSystem commandSystem = scene.getEntityProcessor<CommandSystem>();
-						Command command = new MoveCommand(grabComponent.target, offset) { playerInitiated = false };
-						commandSystem.QueueCommand(command);
-					}
+					if (entity.HasComponent<GrabComponent>() && entity.getComponent<GrabComponent>().isGrabbing)
+						ManipulateEntity(entity, moveComponent);
 				}
             }
         }
-    }
+
+		/// <summary>
+		/// Pushes / Pulls an entity with the player when grabbing.
+		/// </summary>
+		private void ManipulateEntity(Entity grabber, MoveComponent moveComponent) {
+			GrabComponent grabComponent = grabber.getComponent<GrabComponent>();
+			MoveComponent grabbedMoveComponent = grabComponent.target.getComponent<MoveComponent>();
+
+			Point offset = moveComponent.position - grabbedMoveComponent.position;
+			if (offset == Point.Zero) // Player overlaps with target when pushing forward
+				offset = Utilities.Direction.DirectionPointOffset(grabber.getComponent<RotateComponent>().direction);
+			else // Otherwise the grabber is pulling, not pushing
+				offset = offset.Normalize();
+
+			CommandSystem commandSystem = scene.getEntityProcessor<CommandSystem>();
+			Command command = new MoveCommand(grabComponent.target, offset) { playerInitiated = false };
+			commandSystem.QueueCommand(command);
+		}
+	}
 }
--- GravekeeperReboot/Source/Systems/RotateSystem.cs
+++ GravekeeperReboot/Source/Systems/RotateSystem.cs
@@ -12,25 +12,33 @@ namespace GravekeeperReboot.Source.Systems {
 			base.process(entities);
 			foreach (Entity entity in entities) {
 				RotateComponent rotateComponent = entity.getComponent<RotateComponent>();
-				if (entity.rotationDegrees != Utilities.Direction.DirectionDegrees(rotateComponent.direction)) {
-					entity.setRotationDegrees(Utilities.Direction.DirectionDegrees(rotateComponent.direction));
-					
-					if (entity.getComponent<GrabComponent>() != null && entity.getComponent<GrabComponent>().isGrabbing) {
-						GrabComponent grabComponent = entity.getComponent<GrabComponent>();
-					
-						// Moves target to grabber's position
-						Point offset = Vector2.Normalize(entity.position - grabComponent.target.position).roundToPoint();
-						// Pushes target out in the direction grabber is facing
-						Point playerDirection = Utilities.Direction.DirectionPointOffset(rotateComponent.direction);
+				int targetRotation = Utilities.Direction.DirectionDegrees(rotateComponent.direction);
 
-						CommandSystem commandSystem = scene.getEntityProcessor<CommandSystem>();
-						commandSystem.QueueCommand(
-							new MoveCommand(grabComponent.target, offset) { playerInitiated = false },
-							new MoveCommand(grabComponent.target, playerDirection) { playerInitiated = false }
-						);
-					}
+				if (entity.rotationDegrees != targetRotation) {
+					entity.setRotationDegrees(targetRotation);
+					
+					if (entity.getComponent<GrabComponent>() != null && entity.getComponent<GrabComponent>().isGrabbing)
+						ManipulateEntity(entity, rotateComponent);
 				}
 			}
+		}
+
+		/// <summary>
+		/// Moves an entity along with the player when grabbing
+		/// </summary>
+		private void ManipulateEntity(Entity entity, RotateComponent rotateComponent) {
+			GrabComponent grabComponent = entity.getComponent<GrabComponent>();
+
+			// Moves target to grabber's position
+			Point offset = Vector2.Normalize(entity.position - grabComponent.target.position).roundToPoint();
+			// Pushes target out in the direction grabber is facing
+			Point playerDirection = Utilities.Direction.DirectionPointOffset(rotateComponent.direction);
+
+			CommandSystem commandSystem = scene.getEntityProcessor<CommandSystem>();
+			commandSystem.QueueCommand(
+				new MoveCommand(grabComponent.target, offset) { playerInitiated = false },
+				new MoveCommand(grabComponent.target, playerDirection) { playerInitiated = false }
+			);
 		}
 	}
 }
